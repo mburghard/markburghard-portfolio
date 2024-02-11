6bf807dc8e737221b608c2d83f14f27539eccc48
mburghard <43384519+mburghard@users.noreply.github.com>
2024-02-11 06:36:20 -0800

    Updated routing to use HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
 import ProductShowcasePage from "./pages/ProductShowcase";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Router basename="/markburghard-react-site">
+      <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product-showcase" element={<ProductShowcasePage />} />
