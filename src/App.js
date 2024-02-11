bf8173a8e707339cbc5d0ffb80383acefc198f8d
mburghard <43384519+mburghard@users.noreply.github.com>
2024-02-11 06:21:50 -0800

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
-import FormPage from "./pages/FormPage";
 import ProductShowcasePage from "./pages/ProductShowcase";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -9,10 +8,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/markburghard-react-site">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/advanced-form" element={<FormPage />} />
           <Route path="/product-showcase" element={<ProductShowcasePage />} />
         </Routes>
       </Router>
